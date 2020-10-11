6b4b70d9388ce3244dfee9010cabbe7ad185a596
srobalo <s.robalo@campus.fct.unl.pt>
2020-10-11 16:58:55 +0100

    Update

---
+++ assignment1/TP1.py
@@ -0,0 +1,12 @@
+# TP1 Test & Train File contents
+# Features(4) + ClassLabels(1):
+# 1) Variance 
+# 2) Skewness 
+# 3) Curtosis of Wavelet Transformed image
+# 4) Entropy of the bank note image
+# 5) Class Label [0=RealBankNotes & 1=FakeBankNotes]
+
+# Classifiers(3) needed for the project
+# 1) Logistic Regression
+# 2) Naïve Bayes Classifier using Kernel Density Estimation
+# 3) Gaussian Naïve Bayes classifier
