5b898a890e249590132c12753a35e60fe2a0068d
denisov93 <a.denisov@campus.fct.unl.pt>
2020-10-20 15:05:47 +0100

    nb

--- assignment1/NB.py
+++ assignment1/NB.py
@@ -7,9 +7,12 @@ Created on Mon Oct 19 19:36:15 2020
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.neighbors.kde import KernelDensity
+from sklearn.model_selection import StratifiedKFold,train_test_split
 from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+
+
 mat = np.loadtxt("TP1_train.tsv",delimiter='\t')
 data = shuffle(mat)
 Ys = data[:,4].astype(int)
@@ -18,87 +21,54 @@ means = np.mean(Xs,axis=0)
 stdevs = np.std(Xs,axis=0)
 Xs = (Xs-means)/stdevs
 
+def make_hists(data,features):
+    hists = []
+    for feat in features:
+        hists.append(np.ones(len(feat)))
+    for ix in range(len(hists)):
+        hists[ix] = np.log(hists[ix]/float(data.shape[0]+len(features[ix])))
+    return hists
+    
+def split_data(features,test_fraction): 
+     oks = Xs[Ys==0,:]
+     noks = Xs[Ys==1,:]
+     e_test_points = int(test_fraction*oks.shape[0])
+     e_train = oks[e_test_points:,:]
+     e_test = oks[:e_test_points,:]
+     p_test_points = int(test_fraction*noks.shape[0])
+     p_train = noks[p_test_points:,:]
+     p_test = noks[:p_test_points,:]
+     return e_train,p_train,e_test,p_test
 
-#aditive smoth
-smoth = []
-for feat in range(0,4):
-    print(Xs[feat])
+def classify(e_class,e_log,p_class,p_log,feat_mat):
+     classes = np.zeros(feat_mat.shape[0])
+     for row in range(feat_mat.shape[0]):
+         e_sum = e_log
+         p_sum = p_log
+         for column in range(feat_mat.shape[1]):
+             e_sum = e_sum + e_class[column][int(feat_mat[row,column])]
+             p_sum = p_sum + p_class[column][int(feat_mat[row,column])]
+         if e_sum<p_sum:
+             classes[row]=1
+     return classes
 
-X = Xs
-kde = KernelDensity(kernel='gaussian', bandwidth=1).fit(X)
-score = kde.score_samples(X)
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#Tireado da net para referencias
-
-# Example of calculating class probabilities
-from math import sqrt
-from math import pi
-from math import exp
-
-# Split the dataset by class values, returns a dictionary
-def separate_by_class(dataset):
-	separated = dict()
-	for i in range(len(dataset)):
-		vector = dataset[i]
-		class_value = vector[-1]
-		if (class_value not in separated):
-			separated[class_value] = list()
-		separated[class_value].append(vector)
-	return separated
-
-# Calculate the mean of a list of numbers
-def mean(numbers):
-	return sum(numbers)/float(len(numbers))
-
-# Calculate the standard deviation of a list of numbers
-def stdev(numbers):
-	avg = mean(numbers)
-	variance = sum([(x-avg)**2 for x in numbers]) / float(len(numbers)-1)
-	return sqrt(variance)
-
-# Calculate the mean, stdev and count for each column in a dataset
-def summarize_dataset(dataset):
-	summaries = [(mean(column), stdev(column), len(column)) for column in zip(*dataset)]
-	del(summaries[-1])
-	return summaries
-
-# Split dataset by class then calculate statistics for each row
-def summarize_by_class(dataset):
-	separated = separate_by_class(dataset)
-	summaries = dict()
-	for class_value, rows in separated.items():
-		summaries[class_value] = summarize_dataset(rows)
-	return summaries
-
-# Calculate the Gaussian probability distribution function for x
-def calculate_probability(x, mean, stdev):
-	exponent = exp(-((x-mean)**2 / (2 * stdev**2 )))
-	return (1 / (sqrt(2 * pi) * stdev)) * exponent
-
-# Calculate the probabilities of predicting each class for a given row
-def calculate_class_probabilities(summaries, row):
-	total_rows = sum([summaries[label][0][2] for label in summaries])
-	probabilities = dict()
-	for class_value, class_summaries in summaries.items():
-		probabilities[class_value] = summaries[class_value][0][2]/float(total_rows)
-		for i in range(len(class_summaries)):
-			mean, stdev, _ = class_summaries[i]
-			probabilities[class_value] *= calculate_probability(row[i], mean, stdev)
-	return probabilities
-
-# Test calculating class probabilities
-dataset =  [[3.393533211,2.331273381,0],
-	[3.110073483,1.781539638,0],
-	[1.343808831,3.368360954,0],
-	[3.582294042,4.67917911,0],
-	[2.280362439,2.866990263,0],
-	[7.423436942,4.696522875,1],
-	[5.745051997,3.533989803,1],
-	[9.172168622,2.511101045,1],
-	[7.792783481,3.424088941,1],
-	[7.939820817,0.791637231,1]]
-summaries = summarize_by_class(dataset)
-probabilities = calculate_class_probabilities(summaries, dataset[0])
-print(probabilities)
+def do_bayes():
+     features = Xs
+     
+     e_train,p_train,e_test,p_test = split_data(features,0.33)
+     e_hists = make_hists(e_train,features)
+     print(e_train,p_train,e_test,p_test)
+     p_hists = make_hists(p_train,features)
+     tot_len = e_train.shape[0]+p_train.shape[0]
+     e_log = np.log(float(e_train.shape[0])/tot_len)
+     p_log = np.log(float(p_train.shape[0])/tot_len)
+     c_e = classify(e_hists,e_log,p_hists,p_log,e_test)
+     c_p = classify(e_hists,e_log,p_hists,p_log,p_test)
+     errors = sum(c_e)+sum(1-c_p)
+     error_perc = float(errors)/(len(c_e)+len(c_p))*100
+     print(f'{errors:.0f} errors; {error_perc:.2f}% error rate')
+     print('\tE\tP')
+     print(f'E\t{sum(1-c_e):.0f}\t{sum(1-c_p):.0f}')
+     print(f'P\t{sum(c_e):.0f}\t{sum(c_p):.0f}')
+ 
+do_bayes()
