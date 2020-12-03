a58c00dcaafcbfc97bc4614cd8f2c9a4b73fd724
srobalo <s.robalo@campus.fct.unl.pt>
2020-12-03 09:39:21 +0000

    Update

--- assignment2/tp2.py
+++ assignment2/tp2.py
@@ -6,4 +6,26 @@ AA 20/21
 TP4 Instructor: Joaquim Francisco Ferreira da Silva
 Regency: Ludwig Krippahl
 '''
+'''
+Goal: 
+The goal of this assignment is to examine a set of bacterial cell images 
+using machine learning techniques, including feature extraction, 
+features selection and clustering, in order to help the biologists organize similar images.
+'''
+'''
+Resources: 
+A set of 563 PNG images (in the images/ folder) 
+taken from a super-resolution fluorescence microscopy photograph of Staphylococcus aureus.
+All images have the same dimensions, 50 by 50 pixels, 
+with a black background and the segmented region centered in the image. 
+'''
+'''
+In this assignment, you will load all images, extract features, 
+examine them and select a subset for clustering with the goal of 
+reaching some conclusion about the best way of grouping these images.
+'''
+
+#imports
+import tp2_aux as aux 
+#
 
